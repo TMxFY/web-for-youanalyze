6dcfd57ca94e509d3a22b71a84a16293702b48d3
TMxFY <95030334+TMxFY@users.noreply.github.com>
2023-10-19 09:06:07 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#FF00FF";
 
-let btn = document.getElementById("btn");
+// let btn = document.getElementById("btn");
 
-btn.addEventListener("click",() => {
-    tg.MainButton.setText("Сообщение отправлено!");
-    tg.MainButton.show();
-    tg.sendData("sendTestMessage");
-});
+// btn.addEventListener("click",() => {
+//     tg.MainButton.setText("Сообщение отправлено!");
+//     tg.MainButton.show();
+//     tg.sendData("sendTestMessage");
+// });
